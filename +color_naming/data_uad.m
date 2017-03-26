48a5cc26baa4c760f85a7df48c47cb6713fb884f
bps10 <brian.patrick.schmidt@gmail.com>
2017-03-26 11:18:14 -0700

    doc and fix find_cross, add arcsine_transform

--- +color_naming/data_uad.m
+++ +color_naming/data_uad.m
@@ -1,8 +1,12 @@
-function [x, y] = data_uad(dat, names)
+function [x, y] = data_uad(dat, names, arcsine_transform)
     % Transform data into uniform appearance diagram coordinates. Currently
     % there is no arcsine transformation. That transformation would only
     % apply to traditional hue scaling data. The x,y values returned range
     % from -1 to 1.
+
+    if nargin < 3
+        arcsine_transform = false;
+    end
     
     % find indices of hue names: must correspond to columns in dat
     findstr = @(s) find(not(cellfun('isempty', strfind(names, s))));
@@ -15,14 +19,26 @@ function [x, y] = data_uad(dat, names)
         indices(5) = findstr('yellow');
     catch Me
         error('names variable must contain red, green, blue and yellow');
+    end   
+    
+    if arcsine_transform
+        % arcsine transform data and then find which cols correspond to
+        % which colors
+        hues = color_naming.arcsine_transform(dat);
+        red = hues(:, indices(2));
+        green = hues(:, indices(3));
+        blue = hues(:, indices(4));
+        yellow = hues(:, indices(5));
+        white = hues(:, indices(1));
+    else
+        % convert numbers to red, green, blue, yellow, white
+        red = sum(dat == indices(2), 2);
+        green = sum(dat == indices(3), 2);
+        blue = sum(dat == indices(4), 2);
+        yellow = sum(dat == indices(5), 2);
+        white = sum(dat == indices(1), 2);              
     end
     
-    % convert numbers to red, green, blue, yellow, white
-    red = sum(dat == indices(2), 2);
-    green = sum(dat == indices(3), 2);
-    blue = sum(dat == indices(4), 2);
-    yellow = sum(dat == indices(5), 2);
-    white = sum(dat == indices(1), 2);
     % compute the total for each observation (w, r, g, b, y);
     tot = red + green + blue + yellow + white;
     
