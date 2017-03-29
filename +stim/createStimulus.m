f2de46b08d7e7f8910818473be55b673888badd0
bps10 <brian.patrick.schmidt@gmail.com>
2017-03-29 11:36:23 -0700

    document createStimulus

--- +stim/createStimulus.m
+++ +stim/createStimulus.m
@@ -1,36 +1,55 @@
-function createStimulus(stimsize, stimshape, powers)
-    import stim.*
+function createStimulus(stimsize, stimshape, intensities)
+    % Create stimuli. Default will create a zero stimulus for frame2, cross
+    % for frame3 and 100% increment for frame4.
+    %
+    % USAGE
+    % createStimulus(stimsize, stimshape, powers)
+    %    
+    % INPUT
+    % stimsize:     in pixels
+    % stimshape:    char. currently supports square or circle
+    % intensity:    stimulus intensity. values should be between 0
+    %               and 1. powers can be a single value or an array of
+    %               values, in which case a bmp file will be created for
+    %               each power.
+    %
+    % OUTPUT
+    % saves bmp files into tempStimulus directory
+    %
     if nargin<3
-        powers = 1;
+        intensities = 1;
     end
+    
     % cycle through powers and create stimuli for each
     frameN = 4;
-    for p = 1:length(powers)
-        power = powers(p);
+    for p = 1:length(intensities)
+        intensity = intensities(p);
         if strcmp(stimshape, 'square')
             stim_im = zeros(stimsize, stimsize);
-            stim_im(1:end,1:end) = power;
+            stim_im(1:end,1:end) = intensity;
 
         elseif strcmp(stimshape, 'circle')
-            xp =  -fix(stimsize / 2)  : fix(stimsize / 2);
+            xp = -fix(stimsize / 2):fix(stimsize / 2);
             [x, y] = meshgrid(xp);
-            stim_im = (x .^ 2 + y .^ 2) <= (round(stimsize / 2)) .^ 2; 
-        end    
+            stim_im = (x .^ 2 + y .^ 2) <= (round(stimsize / 2)) .^ 2;
+            stim_im = stim_im .* intensity;
+        end
+        % write to file
         imwrite(stim_im, fullfile(pwd, 'tempStimulus', ...'
             ['frame' num2str(frameN) '.bmp']));
         frameN = frameN + 1;
     end
 
-    %Make cross in IR channel to record stimulus location
-    ir_im = stim.create_cross_img(21, 5, true);
-
     if isdir(fullfile(pwd, 'tempStimulus')) == 0;
         mkdir(fullfile(pwd, 'tempStimulus'));
     end
-
-    blank_im = zeros(10, 10);
     
+    % make a blank (zero value) stimulus
+    blank_im = zeros(10, 10);
     imwrite(blank_im, fullfile(pwd, 'tempStimulus', 'frame2.bmp'));
+    
+    %Make cross in IR channel to record stimulus location
+    ir_im = stim.create_cross_img(21, 5, true);    
     imwrite(ir_im, fullfile(pwd, 'tempStimulus', 'frame3.bmp'));
     
 
