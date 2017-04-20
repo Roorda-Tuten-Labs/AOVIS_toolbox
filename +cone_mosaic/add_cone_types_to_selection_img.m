fa868f22f5f5e5ae6a63bac9e8ad884534abe4c4
bps10 <brian.patrick.schmidt@gmail.com>
2017-04-20 13:19:37 -0700

    made register_cones a stand alone file

--- +cone_mosaic/add_cone_types_to_selection_img.m
+++ +cone_mosaic/add_cone_types_to_selection_img.m
@@ -1,13 +1,27 @@
 function handle = add_cone_types_to_selection_img(subject, selection_img, ...
     xy_cross_loc)
+    % Add previously identified cone locations (w/ spectrial ID) to a new
+    % image.
+    %
+    % USAGE
     % add_cone_types_to_selection_img(subject, selection_img, xy_cross_loc)
     %
+    % INPUT
+    % subject           subject ID
+    % selection_img     image to add identified locations to
+    % xy_cross_loc      optional. add the location of a delivery cross to
+    %                   the selection_img. this feature is useful for
+    %                   determining location of selection area relative to
+    %                   the cones of interest.
+    %
+    % OUTPUT
+    % handle            figure handle.
     %
     
     if nargin < 3
         xy_cross_loc = [];
     end
-    % this is for testing the routine
+    % this is directory to reference images and cone database
     curdir = fileparts(mfilename('fullpath'));
 
     % load reference image
@@ -23,6 +37,7 @@ function handle = add_cone_types_to_selection_img(subject, selection_img, ...
         selection_img = selection_img(:, :, 1);
     end
 
+    % crop the image down
     %buffered_img = img.zero_buffer(selection_img, size(ref));
     if size(selection_img, 1) > size(ref, 1) || size(selection_img, 2) > size(ref, 2)
         center = floor(size(selection_img) ./ 2);
@@ -38,67 +53,11 @@ function handle = add_cone_types_to_selection_img(subject, selection_img, ...
         img_crop = imcrop(selection_img, croprect);
     end
 
-    % output =  [error,diffphase,net_row_shift,net_col_shift]
-    [output, ~] = img.dftregistration(fft2(ref), fft2(img_crop), 1);
+    % register the cones with old coords
+    [cones_in_new_coords, nn_cone_coords] = cone_mosaic.register_cones(...
+        ref, ref_cone_coords, img_crop);
     
-    [x_interest, y_interest, ~] = img.find_cones(7, img_crop, 'auto', 0.6, 0);
-    
-    % relative offsets (in pixels) between the two images
-    offsets = output(3:4);
-    
-    selection_img_coords = [x_interest, y_interest];
-    selection_img_ref_coords = [x_interest + offsets(2), ...
-        y_interest + offsets(1)];
-
-    [index, distance] = knnsearch(ref_cone_coords(:, 1:2), ...
-        selection_img_ref_coords);
-    
-    % now iterate through each possible match and select only those higher
-    % than the threshold (2-7 pix).
-    cone_coords = zeros(size(ref_cone_coords));
-    unique_indexes = unique(index);
-    for c = 1:length(unique_indexes)        
-        ref_ind = unique_indexes(c);
-        inds = find(index == ref_ind);
-        dists = distance(inds);
-        % check if smallest distance is below threshold
-        [mindist, ind] = min(dists);
-        if mindist < 7
-            selection_ind = inds(ind);
-            cone_coords(c, 1:2) = selection_img_coords(selection_ind, :);
-            cone_coords(c, 3) = ref_cone_coords(ref_ind, 3);
-            cone_coords(c, 4) = ref_ind;
-        end
-    end
-    cone_coords = util.remove_zero_rows(cone_coords);
-    
-    % set up coord spaces, then translate ref into selection coords
-    sel_coords = cone_coords(:, 1:2);
-    ref_coords = ref_cone_coords(cone_coords(:, 4), 1:2);
-   
-    ref_coords(:, 1) = ref_coords(:, 1) - offsets(2);
-    ref_coords(:, 2) = ref_coords(:, 2) - offsets(1);  
-    
-    % solve linear equation
-    transform_matrix = sel_coords \ ref_coords;
-
-%     ref_coords = transform_matrix * ref_coords';
-%     ref_coords = ref_coords';    
-%    
-%     figure; hold on;
-%     plot(sel_coords(:, 1), sel_coords(:, 2), 'k.')
-%     plot(ref_coords(:, 1), ref_coords(:, 2), 'ro')
-
-    % translate
-    ref_cone_coord = ref_cone_coords(:, 1:2);
-    ref_cone_coord(:, 1) = ref_cone_coord(:, 1) - offsets(2);
-    ref_cone_coord(:, 2) = ref_cone_coord(:, 2) - offsets(1); 
-
-    % scale and rotate
-    class_cones_in_new_coords = transform_matrix * ref_cone_coord(:, 1:2)';
-    class_cones_in_new_coords = class_cones_in_new_coords';
-    
-    % plot the results
+    % --- plot the results --- %
     handle = figure;
     if max(max(img_crop)) > 125
         imshow(img_crop, [0 255]);
@@ -108,21 +67,21 @@ function handle = add_cone_types_to_selection_img(subject, selection_img, ...
     
     hold on;
     colors = {'b' 'g' 'r'};
-    for c = 1:size(class_cones_in_new_coords, 1)
-        plot(class_cones_in_new_coords(c, 1), ...
-            class_cones_in_new_coords(c, 2), '.', ...
+    % this adds all cones of interest after transformation into new
+    % coordinates.
+    for c = 1:size(cones_in_new_coords, 1)
+        plot(cones_in_new_coords(c, 1), ...
+            cones_in_new_coords(c, 2), '.', ...
             'color', colors{ref_cone_coords(c, 3)}, 'markersize', 12)
     end   
     
-    for c = 1:size(cone_coords, 1)
-        plot(cone_coords(c, 1), cone_coords(c, 2), 'o', ...
-            'color', colors{cone_coords(c, 3)})
+    % cones as found by matching them w/ nearest neighbor
+    % this process will not find 10-20% of cones, but of those that it does
+    % find, they will be properly centered on the cone.
+    for c = 1:size(nn_cone_coords, 1)
+        plot(nn_cone_coords(c, 1), nn_cone_coords(c, 2), 'o', ...
+            'color', colors{nn_cone_coords(c, 3)})
     end       
-        
-%     figure; hold on;
-%     plot(cone_coords(:, 1), cone_coords(:, 2), 'r.')
-%     plot(class_cones_in_new_coords(:, 1), ...
-%         class_cones_in_new_coords(:, 2), 'k.')
 
     if ~isempty(xy_cross_loc)
         plot(xy_cross_loc(1), xy_cross_loc(2), 'y+', 'markersize', 25);
--- tests/test_add_cone_types_to_selection_img.m
+++ tests/test_add_cone_types_to_selection_img.m
@@ -1,14 +1,13 @@
 % test_add_cone_types_to_selection_img
 clearvars;
- 
-testdir = fileparts(which('cone_mosaic.add_cone_types_to_selection_img'));
 
+% test image number (select 1-5)
+img_n = 5;
+% read in test image
+testdir = fileparts(which('cone_mosaic.add_cone_types_to_selection_img'));
 fname = fullfile(testdir, 'cone_identify', 'test', ...
-    'sumnorm_20076R_5.tif');
+    ['sumnorm_20076R_' num2str(img_n) '.tif']);
 selection_img = imread(fname);
-%selection_img = imread('cone_identify/test/20076R_1.png');
-%selection_img = imresize(selection_img, 0.2);
-%selection_img = uint8(selection_img(:, :, 1));
 xy_cross_loc = [300 400];
     
 cone_mosaic.add_cone_types_to_selection_img('20076R', selection_img,...
