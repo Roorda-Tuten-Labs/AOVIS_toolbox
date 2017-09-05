e43a6bb16e785ce9a7e61a89e5703f771c2ab1c8
Brian Schmidt <brian.patrick.schmidt@gmail.com>
2017-09-05 15:43:56 -0700

    switched multi-cone exp to render a stimulus of a fixed size

--- +stim/create_Ncone_stim.m
+++ +stim/create_Ncone_stim.m
@@ -1,7 +1,11 @@
-function create_Ncone_stim(locs_xy, intensity, stimsize, first_frameN)
-% create_Ncone_stim(locs_xy, intensity, stimsize, first_frameN)
+function create_Ncone_stim(locs_xy, max_locs_xy, intensity, stimsize, ...
+    first_frameN)
+% create_Ncone_stim(locs_xy, max_locs_xy, intensity, stimsize, ...
+%    first_frameN)
+%
+%
 
-if nargin < 4
+if nargin < 5
     first_frameN = 4;
 end
 
@@ -9,24 +13,25 @@ end
 if mod(stimsize, 2) == 0
     stimsize = stimsize + 1;
 end
+% halfwidth of stimulus
+deltasize = floor(stimsize / 2);
 
-deltasize = (stimsize - 1) / 2;
-
-locs_xy(:, 1) = locs_xy(:, 1) - (min(locs_xy(:, 1)) - deltasize - 1);
-locs_xy(:, 2) = locs_xy(:, 2) - (min(locs_xy(:, 2)) - deltasize - 1);
-
-% Find the furthest away points.
-image_size = max(locs_xy(:));
-
-% Add each location (cone) to the stimulus image to be displayed
-stimulus = zeros(image_size, image_size);
+% add each location (cone) to the stimulus image to be displayed
+stimulus = zeros(max_locs_xy(1), max_locs_xy(2));
 for loc = 1:size(locs_xy)
+    % find x position
     x = locs_xy(loc, 1);
     xs = x - deltasize:x + deltasize;   
+    % find y position
     y = locs_xy(loc, 2);
     ys = y - deltasize:y + deltasize;   
+    
+    % add location to stimulus with given intensity
     stimulus(xs, ys) = intensity;
 end
+
+% change from x, y cartesian space to image space with 0,0 at top left.
+stimulus = flipud(rot90(stimulus));
 
 %imshow(stimulus);
 
--- +stim/create_Ncone_stim_routine.m
+++ +stim/create_Ncone_stim_routine.m
@@ -1,61 +1,44 @@
-%%
-first_frameN = 4;
-
-spotsize = 3;
-% force spot size to be odd.
-if mod(spotsize, 2) == 0
-    spotsize = spotsize + 1;
-end
-
-deltasize = (spotsize - 1) / 2;
-
-xi = [256 252 260 248 264 252 260]';
-yi = [256 249 249 256 256 263 263]';
-
-cone_locs = [xi, yi];
-
-cone_locs(:, 1) = cone_locs(:, 1) - (min(cone_locs(:, 1)) - deltasize - 1);
-cone_locs(:, 2) = cone_locs(:, 2) - (min(cone_locs(:, 2)) - deltasize - 1);
-
-% Find the furthest away points.
-image_size = max(cone_locs(:));
-
+function create_Ncone_stim_routine(locs_xy, intensity, stimsize, first_frameN)
+% create_Ncone_stim(locs_xy, intensity, stimsize, first_frameN)
 %
-figure;
-subplot(6, 6, 1);
+%
 
-ncones = 7;
-k = 3;
-combos = combnk(1:ncones, k);
-ncombos = length(combos);
-
-for s = 1:ncombos
-    stimulus = zeros(image_size, image_size);
-    cones = combos(s, :);
-    for n = cones
-        x = cone_locs(n, 1);
-        xs = x - deltasize:x + deltasize;   
-        y = cone_locs(n, 2);
-        ys = y - deltasize:y + deltasize;   
-        stimulus(xs, ys) = 1;
-    end
-    subplot(6, 6, s);
-    imshow(stimulus);
+if nargin < 4
+    first_frameN = 4;
 end
 
-% if isdir(fullfile(pwd, 'tempStimulus')) == 0;
-%     mkdir(fullfile(pwd, 'tempStimulus'));
-%     cd(fullfile(pwd, 'tempStimulus'));
-% else
-%     cd(fullfile(pwd, 'tempStimulus'));
-% end
+% force spot size to be odd.
+if mod(stimsize, 2) == 0
+    stimsize = stimsize + 1;
+end
 
-% % Make cross in IR channel to record stimulus location
-% ir_im0 = stim.create_cross_img(21, 5, true);
-% 
-% blank_im0 = zeros(10, 10);
-% 
-% imwrite(blank_im0, 'frame2.bmp');
-% imwrite(ir_im0, 'frame3.bmp');
-% 
-% cd ..;
+% halfwidth of stimulus
+deltasize = floor(stimsize / 2);
+
+% find pixels in relative space
+locs_xy(:, 1) = locs_xy(:, 1) - (min(locs_xy(:, 1)) - deltasize - 1);
+locs_xy(:, 2) = locs_xy(:, 2) - (min(locs_xy(:, 2)) - deltasize - 1);
+
+% add each location (cone) to the stimulus image to be displayed
+stimulus = zeros(max(locs_xy(:, 1)), max(locs_xy(:, 2)));
+for loc = 1:size(locs_xy)
+    % find x position
+    x = locs_xy(loc, 1);
+    xs = x - deltasize:x + deltasize;   
+    % find y position
+    y = locs_xy(loc, 2);
+    ys = y - deltasize:y + deltasize;   
+    
+    % add location to stimulus with given intensity
+    stimulus(xs, ys) = intensity;
+end
+
+% change from x, y cartesian space to image space with 0,0 at top left.
+stimulus = rot90(stimulus);
+
+%imshow(stimulus);
+
+% save images
+savedir = fullfile(pwd, 'tempStimulus');
+util.check_for_dir(savedir);
+imwrite(stimulus, fullfile(savedir, ['frame' num2str(first_frameN) '.bmp']));
