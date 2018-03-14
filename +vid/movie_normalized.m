064e09b99c2e6b3e25ba8c30e8f9fbf88a82f83a
Brian Schmidt <brian.patrick.schmidt@gmail.com>
2018-03-14 15:36:48 -0700

    modernize movie_norm

--- +vid/movie_normalized.m
+++ +vid/movie_normalized.m
@@ -1,43 +1,67 @@
-%This program will add movie frames together and generate an average image that has been normalized by the the number of frames 
-%that have contributed to each pixel in the image. The result is that there
-%are no intensity drop offs at the edges of the averaged image that would normally be casued by fewere frames contributing to those regions. 
-%On the other hand, the edges of the frames are noisier since they comprose fewer averaged frames.
 
+function movie_normalized
+% movie_normalized()
+%
+% USAGE
+% This program will add movie frames together and generate an average 
+% image that has been normalized by the the number of frames 
+% that have contributed to each pixel in the image. The result is that there
+% are no intensity drop offs at the edges of the averaged image that would 
+% normally be casued by fewere frames contributing to those regions. 
+% On the other hand, the edges of the frames are noisier since they 
+% comprose fewer averaged frames.
+%
 % You only need one copy of this program. You need to identify
 % the folder that contains the avi files. It will add all avi files in the
 % selected directory
-%Austin Roorda October 17, 2007
+%
+% NOTES
+% Austin Roorda October 17, 2007
+% Edited BPS 14 Mar 2018 -- update to work with modern Matlab.
 
-pname = uigetdir;
-a= dir([pname '/*.avi']);
 
-[nummovies junk] = size(a);
+[filenames, path] = uigetfile('*.avi', 'Select video(s)', ...
+    'MultiSelect', 'on');
+
+nummovies = length(filenames);
 
 for movienum = 1:nummovies
-    
-    fname = a(movienum).name
+        
+    vidName = fullfile(path, filenames{movienum});
+    disp(['Summing frames from: ' vidName]);
+    vidFrames = vid.read_video(vidName);    
 
-    AVIdetails=aviinfo([pname '/' fname]);
+    [height, width] = size(vidFrames(1).cdata);
+    sumframe=zeros(height, width);
+    sumframebinary=ones(height, width);
 
-    sumframe=zeros(AVIdetails.Height,AVIdetails.Width);
-    sumframebinary=ones(AVIdetails.Height,AVIdetails.Width);
-
+    %change this range to add any subset of frames
     startframe = 1; 
-    endframe = AVIdetails.NumFrames; %change this range to add any subset of frames
-    %endframe=25;
+    endframe = length(vidFrames); 
+    
+    fprintf('Frames %g to %g are being co-added and saved to a tif file\n',...
+        startframe,endframe);
 
-    fprintf('Frames %g to %g are being co-added and saved to a tif file\n',startframe,endframe);
+    for framenum = startframe:endframe
+        currentframe = double(vidFrames(framenum).cdata);
+        
+        %generate a binary image to locate the actual image pixels
+        currentframebinary = imbinarize(currentframe, 1); 
+        sumframe = sumframe + currentframe;
 
-    for (framenum=startframe:endframe)
-    currentframe = double(frame2im(aviread([pname '/' fname],framenum)));
-    currentframebinary = im2bw(currentframe,1); %generate a binary image to locate the actual image pixels
-    sumframe=sumframe+currentframe;
-    sumframebinary = sumframebinary+currentframebinary; % generate an image to divide by the sum image to generate an average
-    imshow(sumframe./max(max(sumframe)));
-    drawnow;
+        % generate an image to divide by the sum image to generate an average
+        sumframebinary = sumframebinary + currentframebinary; 
+
     end
-    name = [pname '/sumnorm_' fname '.tif']
-    sumframenew = sumframe./sumframebinary;
-    imshow(sumframenew/max(max(sumframenew)));drawnow;
-    imwrite(sumframenew/max(max(sumframenew)),name,'tif','Compression','none');
+    % normalize    
+    sumframenew = sumframe ./ sumframebinary;
+    
+    % save file
+    name = fullfile(path, ['sumnorm_' filenames{movienum} '.tif']);
+    imwrite(sumframenew / max(max(sumframenew)), name, 'tif',...
+        'Compression', 'none');
+    
+    % show image
+    imshow(sumframenew / max(max(sumframenew)));    
+    
 end
