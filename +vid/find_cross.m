8b3075c260192f22c0f6ef060ac0c32143a43b91
William Tuten <wtuten@berkeley.edu>
2021-05-01 16:28:35 -0700

    Update vid.find_cross to define cross type either by channel (i.e. 'ir') or by cross intensity value (i.e. 255).

--- +vid/find_cross.m
+++ +vid/find_cross.m
@@ -1,36 +1,44 @@
 function [X_cross_loc, Y_cross_loc, frames_w_cross] = find_cross(filename, ...
     cross_size_pix, xcorr_threshold, print_output, return_mean_only, ...
-    cross_channel, show_error_dlg)
+    cross_channel_or_val, show_error_dlg)
 % Find cross in a video
 %
 % USAGE
 % [X_cross_loc, Y_cross_loc, frames_w_cross] = find_cross(filename, ...
 %    cross_size_pix, xcorr_threshold, print_output, return_mean_only, ...
-%    cross_channel)
+%    cross_channel_or_val)
 %
 % INPUT
-% filename:         name of video file to read.
-% cross_size_pix:   size of the cross in pixels to use in cross
-%                   correlation.
-% xcorr_threshold:  threshold for detecting a cross. default = 0.6.
-% print_output:     0 or 1. Decide whether to print results
-% return_mean_only: 0 or 1. this routine will find the mean by excluding
-%                   values that are greater than 1 STD from the mean.
-% cross_channel:    the crosses from each channel are encoded with a unique 
-%                   value. we select the cross pixel value for the channel 
-%                   of interest. this will be used below to binarize the 
-%                   image and improve the detection algorithm. Default =
-%                   ir.
-% show_error_dlg:   choose to show an error dialogue box with information
-%                   for the user or not.
-%
+% filename:             name of video file to read.
+% cross_size_pix:       size of the cross in pixels to use in cross
+%                       correlation.
+% xcorr_threshold:      threshold for detecting a cross. default = 0.6.
+% print_output:         0 or 1. Decide whether to print results
+% return_mean_only:     0 or 1. this routine will find the mean by excluding
+%                       values that are greater than 1 STD from the mean.
+% cross_channel_or_val: the crosses from each channel are encoded with a
+%                       unique value. we select the cross pixel value for
+%                       the channel of interest. this will be used below to
+%                       binarize the image and improve the detection
+%                       algorithm. Inputing string options 'ir', 'red', or
+%                       'green' will search for historical cross
+%                       parameters; if a numerical value is passed (0 to
+%                       255), the code with search for that instead.The
+%                       reason for this is that sometimes the cross pixel
+%                       intensities don't adhere to the original options in
+%                       this code. For example, for reasons that aren't
+%                       entirely clear to me, IR cross intensity is
+%                       sometimes 254 rather than 255. Default = 'ir'.
+% show_error_dlg:       choose to show an error dialogue box with information
+%                       for the user or not.
+%   
 % OUTPUT
-% X_cross_loc:      location of cross in X. If return_mean_only==0, this
-%                   will be a vector with values for each frame where the
-%                   cross was found.
-% Y_cross_loc:      location of cross in Y. Same application of
-%                   return_mean_only.
-% frames_w_cross:   array containing frame numbers that contained a cross.
+% X_cross_loc:          location of cross in X. If return_mean_only==0, this
+%                       will be a vector with values for each frame where the
+%                       cross was found.
+% Y_cross_loc:          location of cross in Y. Same application of
+%                       return_mean_only.
+% frames_w_cross:       array containing frame numbers that contained a cross.
 %
 
 
@@ -49,21 +57,25 @@ if nargin < 5
     return_mean_only = true;
 end
 if nargin < 6
-    cross_channel = 'ir';
+    cross_channel_or_val = 'ir';
 end
 if nargin < 7
     show_error_dlg = 1;
 end
 
 % select channel with ir cross and set appropriate cross_pixel_val
-if strcmpi(cross_channel, 'ir');
+if ischar(cross_channel_or_val)
+    if strcmpi(cross_channel_or_val, 'ir')
         cross_pixel_val = 255/255;
-elseif strcmpi(cross_channel, 'red');
+    elseif strcmpi(cross_channel_or_val, 'red')
         cross_pixel_val = 253/255;
-elseif strcmpi(cross_channel, 'green');
+    elseif strcmpi(cross_channel_or_val, 'green')
         cross_pixel_val = 251/255;
+    else
+        error('cross_channel must be set to ir, red or green')
+    end
 else
-    error('cross_channel must be set to ir, red or green')
+    cross_pixel_val = cross_channel_or_val./255; % Search for user-defined cross intensity
 end
 
 % generate a cross
